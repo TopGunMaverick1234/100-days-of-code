e42b6894eec5feb18c5b64f3937eaff83b9ddaf7
TopGunMaverick1234 <winfredmantey62@gmail.com>
2025-06-16 13:36:15 +0000

    First change

---
+++ Day 3.py
@@ -0,0 +1,7 @@
+print("what number do you want to check? ")
+number = int(input("Type your number to see if its odd or even "))
+
+if number % 2 == 0:
+    print("it is an even number")
+else:
+    print("this is an odd number")
